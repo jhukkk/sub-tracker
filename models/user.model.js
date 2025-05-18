7ca3ec9fc3a3ac3cdf0a894956d95da57297bb27
Khulan Janchiv <janchivv.khulan@gmail.com>
2025-05-18 14:51:08 -0700

    Starting controller logic for auth

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'User name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 50,
+    },
+    email: {
+        email: String, 
+        required: [true, 'User Email is required'],
+        trim: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/,'Please fill a valid email address'],
+    },
+    password: {
+        email: String, 
+        required: [true, 'User Password is required'],
+        minLength: 6,
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
